11d013198203d1eb5e47297eb133af0c8726f247
okomeee <marukome.diary@gmail.com>
2019-05-05 18:36:38 +0900

    実装

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,26 @@
+class HomesController < ApplicationController
+
+  def index
+    @posts = Post.all.sample(10)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: 'post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:word)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/index'
+  root "homes#index"
+  post "/posts", to: "homes#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
