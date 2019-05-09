462546ae39eaf800502ca53e12aba2e9cf8b9eca
okomeee <marukome.diary@gmail.com>
2019-05-09 16:27:29 +0900

    Add Token

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,33 +1,43 @@
 class HomesController < ApplicationController
 
   def index
-    @posts = Post.all.sample(100)
-    @post = Post.new
-    @count = Post.count
+    if params[:token] == "7ZRerpJrdSzKzsqm3SFhVcm0cTNhRfqTdhF0pfXHKNDCSgWNqWVXKWT2avXiCjNTbazZ7ovVxJQG4Y-GrFObq_rH0NqMzNs1xLE-_SmX15S-UbjOu8yE6zpssTTWR6lQRwC6kg"
+      @posts = Post.all.sample(100)
+      @post = Post.new
+      @count = Post.count
+    else
+      redirect_to not_certified_path
+    end
   end
 
   def note
-    @posts = Post.all.shuffle
-    @msg = ""
-    @posts.each{|post| @msg = @msg + " " + post.word }
+    if params[:token] == "7ZRerpJrdSzKzsqm3SFhVcm0cTNhRfqTdhF0pfXHKNDCSgWNqWVXKWT2avXiCjNTbazZ7ovVxJQG4Y-GrFObq_rH0NqMzNs1xLE-_SmX15S-UbjOu8yE6zpssTTWR6lQRwC6kg"
+      @posts = Post.all.shuffle
+      @msg = ""
+      @posts.each{|post| @msg = @msg + " " + post.word }
+    else
+      redirect_to not_certified_path
+    end
   end
 
   def create
-    @post = Post.new(post_params)
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to root_path, flash: { success: 'ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!' } }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { redirect_to root_path, flash: { warning: 'きゃ━(;´༎ຶД༎ຶ`)━っっ!!!(失敗...)' } }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if post_params[:token] == "7ZRerpJrdSzKzsqm3SFhVcm0cTNhRfqTdhF0pfXHKNDCSgWNqWVXKWT2avXiCjNTbazZ7ovVxJQG4Y-GrFObq_rH0NqMzNs1xLE-_SmX15S-UbjOu8yE6zpssTTWR6lQRwC6kg"
+      @post = Post.new(word: post_params[:word])
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to root_path(token: "7ZRerpJrdSzKzsqm3SFhVcm0cTNhRfqTdhF0pfXHKNDCSgWNqWVXKWT2avXiCjNTbazZ7ovVxJQG4Y-GrFObq_rH0NqMzNs1xLE-_SmX15S-UbjOu8yE6zpssTTWR6lQRwC6kg"), flash: { success: 'ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!' } }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { redirect_to root_path(token: "7ZRerpJrdSzKzsqm3SFhVcm0cTNhRfqTdhF0pfXHKNDCSgWNqWVXKWT2avXiCjNTbazZ7ovVxJQG4Y-GrFObq_rH0NqMzNs1xLE-_SmX15S-UbjOu8yE6zpssTTWR6lQRwC6kg"), flash: { warning: 'きゃ━(;´༎ຶД༎ຶ`)━っっ!!!(失敗...)' } }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   private
     def post_params
-      params.require(:post).permit(:word)
+      params.require(:post).permit(:word, :token)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'homes/index'
   get "/note", to: 'homes#note'
+  get "/not_certified", to: "homes#not_certified"
   root "homes#index"
   post "/posts", to: "homes#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
